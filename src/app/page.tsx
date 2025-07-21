564a48866f04e37dd486edb4f2cf13c3530fb15e
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2025-07-21 19:58:16 +0200

    fix: apostrophe error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,9 +37,9 @@ export default function App() {
         />
         <div className="lg:hidden text-center lg:fixed lg:text-3xl lg:left-4 lg:right-auto lg:top-2">
           <button className="transition-all text-5xl font-gothic text-shadow-sm text-blue-400 hover:text-red-400 pb-4">Feri Sebestyén</button>
-          <div className="max-w-lg mx-auto pl-8 pr-4 text-left transition-all text-lg text-shadow-sm text-blue-400">Hi! I'm Slovakia based web developer, that loves to create everything from interaction-heavy PWA's through complex admin interfaces.</div>
+          <div className="max-w-lg mx-auto pl-8 pr-4 text-left transition-all text-lg text-shadow-sm text-blue-400">Hi! I&apos;m Slovakia based web developer, that loves to create everything from interaction-heavy PWA&apos;s through complex admin interfaces.</div>
         </div>
-        <p className="hidden lg:block max-w-lg mx-auto pl-8 pr-4 text-center  text-lg text-shadow-sm text-blue-400">Hi!<br></br> I'm Slovakia based web developer, that loves to create everything from interaction-heavy PWA's through complex admin interfaces.</p>
+        <p className="hidden lg:block max-w-lg mx-auto pl-8 pr-4 text-center  text-lg text-shadow-sm text-blue-400">Hi!<br></br> I&apos;m Slovakia based web developer, that loves to create everything from interaction-heavy PWA&apos;s through complex admin interfaces.</p>
         <button className="transition-all hidden lg:block absolute bottom-10 inset-x-0 text-5xl drop-shadow-2xl drop-shadow-neutral-950 text-neutral-50  hover:text-red-400 hover:scale-105">show more</button>
       </div>
     </div>
