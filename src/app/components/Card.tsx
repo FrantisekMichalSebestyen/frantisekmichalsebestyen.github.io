9a2b07ae1e9c189dea97fabc3b2b499b0a56d086
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2025-07-21 19:52:34 +0200

    feat: homepage

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -9,7 +9,7 @@ interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
 const Card = React.forwardRef<HTMLDivElement, CardProps>(({ title, paragraph, style, ...props }, ref) => (
   <div
     ref={ref}
-    className="inline-block font-mono bg-green-50 rounded shadow-md will-change-transform p-8 w-[300px] h-[400px] flex-col items-center justify-center border border-gray-200"
+    className="inline-block font-typewriter bg-green-50 rounded shadow-md will-change-transform p-8 w-[300px] h-[400px] flex-col items-center justify-center border border-gray-200"
     style={style}
     {...props}
   >
