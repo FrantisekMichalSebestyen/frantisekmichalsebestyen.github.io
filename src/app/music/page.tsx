0aa51024be7edd619a0b338d6fd6545af1a6772e
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2025-09-15 22:09:31 +0200

    feat: ui/info improvement

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -96,16 +96,14 @@ export default async function Music() {
 
   return (
     <>
-      <div className="grid grid-cols-2 gap-4 border-2 border-white rounded-lg p-2 items-center">
+      <a href={me.external_urls.spotify}  target="blank" className="group hover:-translate-1 hover:text-pink-400 hover:cursor-pointer hover:bg-neutral-950 hover:border-pink-400 transition-all grid grid-cols-2 gap-4 border-2 border-white rounded-lg p-2 items-center">
         <span className="text-6xl">
           What is
-          <a
-            href={me.external_urls.spotify}
-            target="blank"
-            className="text-pink-400 hover:text-purple-700 hover:cursor-pointer"
+          <span
+            className="text-pink-400 group-hover:text-purple-700 hover:cursor-pointer"
           >
             {` ${me.display_name} `}
-          </a>
+          </span>
           listening to?
         </span>
         <div>
@@ -115,34 +113,49 @@ export default async function Music() {
               alt=""
               width={300}
               height={300}
-              className="object-contai rounded-full w-full h-full border-2 border-white"
+              className="object-contai rounded-full w-full h-full border-2 border-white group-hover:border-pink-400"
             />
           )}
         </div>
-      </div>
-      <div className="border-2 border-white rounded-lg p-2">
+      </a>
+      <div className="border-2 border-white rounded-lg p-2 max-w-full">
         <h2 className="mt-0">Fav Artists</h2>
         <ul>
-          {topArtists.items.sort((a, b) => b.popularity - a.popularity).map((item) => (
-            <li key={item.id}>
+          {topArtists.items.sort((a, b) => b.popularity - a.popularity).map((artist) => (
+            <li key={artist.id}>
               <a
                 className="group hover:text-pink-400 hover:-translate-1 hover:bg-neutral-950 hover:border-pink-400 transition-all border-2 border-white rounded-lg p-2 flex items-center gap-2"
-                href={item.external_urls.spotify}
+                href={artist.external_urls.spotify}
                 target="blank"
               >
-                {item.images?.[0]?.url && (
+                {artist.images?.[0]?.url && (
                   <div className="flex items-center justify-center min-w-32">
                     <Image
                       width={64}
                       height={64}
-                      src={item.images[0]?.url}
+                      src={artist.images[0]?.url}
                       alt=""
                       className="rounded-full w-16 h-16 border-2 border-white group-hover:border-pink-400 "
                     />
                   </div>
                 )}
-                <h2 className="text-4xl text-center grow">{item.name}</h2>
-                <h2 className="text-8xl text-pink-400 group-hover:text-purple-700 px-2">{item.popularity}</h2>
+
+                <div className="flex flex-col text-left grow gap-2">
+                <span className="text-4xl">{artist.name}</span>
+                  <span className="text-left whitespace-nowrap">
+                    {(artist.genres && artist.genres.length > 0) &&
+                    (<div className="max-w-64 overflow-hidden whitespace-nowrap text-ellipsis">
+                      <span className="text-pink-400 group-hover:text-purple-700 max-w-16 overflow-hidden whitespace-nowrap text-ellipsis">Genres: </span>
+                        {artist.genres.map((genre, index) => (
+                      <span key={index}>
+                        <span>{genre}</span>
+                        <span>{index < artist.genres.length - 1 && ', '}</span>
+                      </span>
+                      ))}
+                    </div>)}
+                  </span>
+                </div>
+                <span className="text-8xl text-pink-400 group-hover:text-purple-700 px-2 mb-auto">{artist.popularity}</span>
 
               </a>
             </li>
@@ -155,7 +168,7 @@ export default async function Music() {
           {topTracks.items.sort((a, b) => b.popularity - a.popularity).map((item) => (
             <li key={item.id}>
               <a
-                className="group hover:text-pink-400 hover:-translate-1 hover:bg-neutral-950 hover:border-pink-400 transition-all border-2 border-white rounded-lg p-2 flex items-center gap-2"
+                className="group max-w-full hover:text-pink-400 hover:-translate-1 hover:bg-neutral-950 hover:border-pink-400 transition-all border-2 border-white rounded-lg p-2 flex items-center gap-2"
                 href={item.external_urls.spotify}
                 target="blank"
               >
@@ -171,7 +184,14 @@ export default async function Music() {
                   </div>
                 )}
                 <div className="flex flex-col grow gap-2">
-                  <span className="text-4xl">{item.artists?.[0].name}</span>
+                  <span className="text-ellipsis">
+                    {item.artists && item.artists.map((artist, index) => (
+                      <span key={artist.id}>
+                        <span className="text-4xl">{artist.name} </span>
+                        <span>{index < item.artists.length - 1 && ', '}</span>
+                      </span>
+                    ))}
+                  </span>
                   <span className="text-4xl">{item.name}</span>
                 </div>
                 <span className="text-8xl text-pink-400 group-hover:text-purple-700 px-2 mb-auto">{item.popularity}</span>
