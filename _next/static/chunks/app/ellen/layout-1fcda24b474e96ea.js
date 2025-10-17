2baaf47880e7b5c86c17e564e04860ac109961a4
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2025-10-17 12:08:03 +0200

    Updates

---
+++ _next/static/chunks/app/ellen/layout-1fcda24b474e96ea.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[317,575,586],{2542:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,9967,23))},9967:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[310,441,684,358],()=>s(2542)),_N_E=e.O()}]);
